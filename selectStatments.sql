33fe6e730e9721f76450641dc5552eee38a88c90
sazid-404 <81369160+sazid-404@users.noreply.github.com>
2023-03-09 22:27:00 -0600

    Update selectStatments.sql

--- selectStatments.sql
+++ selectStatments.sql
@@ -1,3 +1,18 @@
- select Ticket_Status from Passenger,Ticket where First_Name = 'John' AND Last_Name = 'Johnson' AND Passenger.SSN == Ticket.PassengerSSN;
- select First_Name, Last_Name from Passenger,TrainStatus,Ticket where Ticket.Train_Date = '2/2/24' AND Ticket_Status = 'Booked';
+ select Ticket_Status 
+ from Passenger,Ticket 
+ where First_Name = 'John' AND Last_Name = 'Johnson' AND Passenger.SSN == Ticket.PassengerSSN;
  
+ select First_Name, Last_Name 
+ from Passenger,TrainStatus,Ticket 
+ where Ticket.Train_Date = '2/2/24' AND Ticket_Status = 'Booked';
+ 
+select Train.Train_Number,Train_Name,Source,Destination,First_Name,Last_Name,Street_Address,Ticket_Type,Ticket_Status 
+from Train, Passenger, TrainStatus, Ticket
+where age >= '50' AND age <= '60' AND Train.Train_Number == TrainStatus.Train_Number;
+
+
+select TrainStatus.Train_Number,Train_Name,Source,Destination,First_Name,Last_Name,Street_Address,Ticket_Type,Ticket_Status 
+from Train, Passenger, TrainStatus, Ticket 
+where age = '-5'AND Train.Train_Number == TrainStatus.Train_Number;
+
+
